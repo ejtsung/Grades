5aa8504b6e69cfe0f366626e5613f3aeaff87423
Eli Tsung <ejtsung@gmail.com>
2018-07-05 13:06:03 -0700

    Initial commit, installed express and mongoose and created index file

---
+++ index/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Test'));
+
+const port = process.env.PORT || 5000;
+
+// To display values of variables to display we use back ticks (ES6)
+app.listen(port, () => console.log(`Server running on port ${port}`));
